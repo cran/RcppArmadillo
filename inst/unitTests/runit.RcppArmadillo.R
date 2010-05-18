3eea19c44dc6c26cf6bc197846f24977d8e61039
Romain, Dirk and Doug <RcppArmadillo-authors@r-enthusiasts.com>
2010-05-18 19:51:19 +0000

    version 0.2.0

--- inst/unitTests/runit.RcppArmadillo.R
+++ inst/unitTests/runit.RcppArmadillo.R
@@ -1,6 +1,6 @@
 #!/usr/bin/r -t
 #
-# Copyright (C) 2010	Dirk Eddelbuettel and Romain Francois
+# Copyright (C) 2010	Dirk Eddelbuettel, Romain Francois and Douglas Bates
 #
 # This file is part of RcppArmadillo.
 #
@@ -17,69 +17,82 @@
 # You should have received a copy of the GNU General Public License
 # along with RcppArmadillo.  If not, see <http://www.gnu.org/licenses/>.
 
+.setUp <- function(){
+    suppressMessages(require(datasets))
+}
+
 test.wrap.R <- function(){
-	res <- .Call( "RcppArmadillo_wrap", PACKAGE = "RcppArmadillo" )
+    res <- .Call( "RcppArmadillo_wrap", PACKAGE = "RcppArmadillo" )
 
-	checkEquals( res[[1]][[1]], matrix(as.integer((diag(3))),nr=3), msg = "eye<imat>(3,3)" )
-	checkEquals( res[[1]][[2]], diag(3), msg = "eye<mat>(3,3)" )
-	checkEquals( res[[1]][[3]], diag(3), msg = "eye<fmat>(3,3)" )
-	checkEquals( res[[1]][[4]], matrix(as.integer((diag(3))),nr=3), msg = "eye<umat>(3,3)" )
+    checkEquals( res[[1]][[1]], matrix(as.integer((diag(3))),nr=3), msg = "eye<imat>(3,3)" )
+    checkEquals( res[[1]][[2]], diag(3), msg = "eye<mat>(3,3)" )
+    checkEquals( res[[1]][[3]], diag(3), msg = "eye<fmat>(3,3)" )
+    checkEquals( res[[1]][[4]], matrix(as.integer((diag(3))),nr=3), msg = "eye<umat>(3,3)" )
 
-	checkEquals( res[[2]][[1]], matrix(0, ncol = 5, nrow=1), msg = "zeros<mat>(5,1)" )
-	checkEquals( res[[2]][[2]], matrix(0, ncol = 5, nrow=1), msg = "zeros<fmat>(5,1)" )
+    checkEquals( res[[2]][[1]], matrix(0, ncol = 5, nrow=1), msg = "zeros<mat>(5,1)" )
+    checkEquals( res[[2]][[2]], matrix(0, ncol = 5, nrow=1), msg = "zeros<fmat>(5,1)" )
 
-	checkEquals( res[[3]][[1]], matrix(0, ncol = 1, nrow=5), msg = "zeros<mat>(1,5)" )
-	checkEquals( res[[3]][[2]], matrix(0, ncol = 1, nrow=5), msg = "zeros<mat>(1,5)" )
+    checkEquals( res[[3]][[1]], matrix(0, ncol = 1, nrow=5), msg = "zeros<mat>(1,5)" )
+    checkEquals( res[[3]][[2]], matrix(0, ncol = 1, nrow=5), msg = "zeros<mat>(1,5)" )
 
-	checkEquals( res[[4]][[1]], matrix(0:3, ncol = 2, nrow=2), msg = "field<int>" )
-	checkEquals( res[[4]][[2]], matrix(letters[1:4], ncol = 2, nrow=2), msg = "field<std::string>" )
+    checkEquals( res[[4]][[1]], matrix(0:3, ncol = 2, nrow=2), msg = "field<int>" )
+    checkEquals( res[[4]][[2]], matrix(letters[1:4], ncol = 2, nrow=2), msg = "field<std::string>" )
 }
 
 test.wrap.Glue <- function(){
-	res <- .Call( "RcppArmadillo_wrap_Glue", PACKAGE = "RcppArmadillo" )
-	checkEquals( res[[1]], 2*diag(3), msg = "wrap(Glue)" )
+    res <- .Call( "RcppArmadillo_wrap_Glue", PACKAGE = "RcppArmadillo" )
+    checkEquals( res[[1]], 2*diag(3), msg = "wrap(Glue)" )
 }
 
 test.wrap.Op <- function(){
-	res <- .Call( "RcppArmadillo_wrap_Op", PACKAGE = "RcppArmadillo" )
-	checkEquals( res[[1]], -1*diag(3), msg = "wrap(Op)" )
+    res <- .Call( "RcppArmadillo_wrap_Op", PACKAGE = "RcppArmadillo" )
+    checkEquals( res[[1]], -1*diag(3), msg = "wrap(Op)" )
 }
 
 test.as.Mat <- function(){
-
-	integer_mat <- matrix( as.integer(diag(4)), ncol = 4, nrow = 4 )
-	numeric_mat <- diag(5)
-	res <- .Call( "RcppArmadillo_as_Mat",
-		list( integer_mat, numeric_mat ),
-		PACKAGE = "RcppArmadillo" )
-	checkEquals( unlist( res), c(4L, 5L, 4L, 5L ), msg = "as<Mat>" )
+    integer_mat <- matrix( as.integer(diag(4)), ncol = 4, nrow = 4 )
+    numeric_mat <- diag(5)
+    res <- .Call( "RcppArmadillo_as_Mat",
+                 list( integer_mat, numeric_mat ),
+                 PACKAGE = "RcppArmadillo" )
+    checkEquals( unlist( res), c(4L, 5L, 4L, 5L ), msg = "as<Mat>" )
 }
 
 test.as.Col <- function(){
-	res <- .Call( "RcppArmadillo_as_Col",
-	      list( 1:10, as.numeric(1:10) ),
-	      PACKAGE = "RcppArmadillo" )
-	checkEquals( unlist( res ), rep(55.0, 4 ), msg = "as<Col>" )
+    res <- .Call( "RcppArmadillo_as_Col",
+                 list( 1:10, as.numeric(1:10) ),
+                 PACKAGE = "RcppArmadillo" )
+    checkEquals( unlist( res ), rep(55.0, 4 ), msg = "as<Col>" )
 }
 
 test.as.Row <- function(){
-	res <- .Call( "RcppArmadillo_as_Row",
-	      list( 1:10, as.numeric(1:10) ),
-	      PACKAGE = "RcppArmadillo" )
-	checkEquals( unlist( res ), rep(55.0, 4 ), msg = "as<Row>" )
+    res <- .Call( "RcppArmadillo_as_Row",
+                 list( 1:10, as.numeric(1:10) ),
+                 PACKAGE = "RcppArmadillo" )
+    checkEquals( unlist( res ), rep(55.0, 4 ), msg = "as<Row>" )
 }
 
 test.fastLm <- function() {
     data(trees)
     flm <- .Call("fastLm",
                  log(trees$Volume),
-                 cbind(rep(1,31), log(trees$Girth)),
+                 cbind(1, log(trees$Girth)),
                  PACKAGE="RcppArmadillo")
     fit <- lm(log(Volume) ~ log(Girth), data=trees)
 
-    checkEquals( as.numeric(flm$coef), as.numeric(coef(fit)),
+    checkEquals(as.numeric(flm$coefficients), as.numeric(coef(fit)),
                 msg="fastLm.coef")
-    checkEquals( as.numeric(flm$stderr), as.numeric(coef(summary(fit))[,2]),
+    checkEquals(as.numeric(flm$stderr), as.numeric(coef(summary(fit))[,2]),
                 msg="fastLm.stderr")
+}
+
+test.fastLm.formula <- function() {
+    data(trees)
+    flm <- fastLm(log(Volume) ~ log(Girth), data=trees)
+    fit <- lm(log(Volume) ~ log(Girth), data=trees)
+
+    checkEquals(flm$coefficients, coef(fit), msg="fastLm.formula.coef")
+    checkEquals(as.numeric(flm$stderr), as.numeric(coef(summary(fit))[,2]),
+                msg="fastLm.formula.stderr")
 }
 
--- src/RcppArmadillo.cpp
+++ src/RcppArmadillo.cpp
@@ -2,7 +2,7 @@
 //
 // RcppArmadillo.cpp: Rcpp/Armadillo glue
 //
-// Copyright (C)  2010 Dirk Eddelbuettel and Romain Francois
+// Copyright (C)  2010 Dirk Eddelbuettel, Romain Francois and Douglas Bates
 //
 // This file is part of RcppArmadillo.
 //
@@ -22,90 +22,58 @@
 #include <RcppArmadillo.h>
 
 using namespace Rcpp ;
-using namespace arma ;
-	
+
 extern "C" SEXP RcppArmadillo_wrap(){
 	
-    List output(4); 
-    List matrices(4) ;
-    List rows(2) ;
-    List cols(2) ;
+    // using the Named(.) = . notation
+    List cols = List::create( 
+    	Named( "Col<double>" ) = arma::zeros<arma::mat>(5,1), 
+    	Named( "Col<float>" )  = arma::zeros<arma::fmat>(5,1)
+    	) ;
+    
+    // using the Named(., .)  notation
+    List rows = List::create( 
+    	Named( "Row<double>",  arma::zeros<arma::mat>(1,5)  ),
+    	Named( "Row<float>" ,  arma::zeros<arma::fmat>(1,5) )
+    	) ;
 	
-    imat x1 = eye<imat>( 3,3 ) ;
-    // x1.swap_rows(0,1)  ;
-    matrices[0] = x1 ;
-	
-    mat x2 = eye<mat>( 3,3 ) ;
-    // x2.swap_rows(0,1)  ;
-    matrices[1] = x2 ;
-	
-    fmat x3 = eye<fmat>( 3, 3 );
-    // x3.swap_rows(0,1)  ;
-    matrices[2] = x3 ;
-	
-    umat x4 = eye<umat>( 3, 3 );
-    // x4.swap_rows(0,1)  ;
-    matrices[3] = x4 ;
-	
-    colvec r1 = zeros<mat>(5,1);
-    cols[0] = r1  ;
-	
-    fcolvec r2 = zeros<fmat>(5,1);
-    cols[1] = r2  ;
-	
-    rowvec c1 = zeros<mat>(1,5);
-    rows[0] = c1  ;
-	
-    frowvec c2 = zeros<fmat>(1,5);
-    rows[1] = c2  ;
-	
-    std::vector<std::string> names(4) ;
-    names[0] = "Mat<int>" ;
-    names[1] = "Mat<double>" ;
-    names[2] = "Mat<float>" ;
-    names[3] = "Mat<unsigned int>" ;
-    matrices.names() = names ;
-	
-    names.resize(2) ;
-    names[0] = "Row<double>" ;
-    names[1] = "Row<float>" ;
-    rows.names() = names ;
-	
-    names[0] = "Col<double>" ;
-    names[1] = "Col<float>" ;
-    cols.names() = names ;
-	
+    // using the _[.] = . notation
+    List matrices = List::create( 
+    	_["Mat<int>"]          = arma::eye<arma::imat>( 3,3 ), 
+    	_["Mat<double>"]       = arma::eye<arma::mat>( 3,3 ),
+    	_["Mat<float>"]        = arma::eye<arma::fmat>( 3, 3 ), 
+    	_["Mat<unsigned int>"] = arma::eye<arma::umat>( 3, 3 )
+    	) ;
+    
+    // creating an empty list and grow it on demand
     List fields ;
-    field<int> f1( 2, 2 ) ;
+    arma::field<int> f1( 2, 2 ) ;
     f1( 0, 0 ) = 0 ; 
     f1( 1, 0 ) = 1 ; 
     f1( 0, 1 ) = 2 ; 
     f1( 1, 1 ) = 3 ; 
     fields["field<int>"] = f1 ;
-    field<std::string> f2(2,2) ;
+    
+    arma::field<std::string> f2(2,2) ;
     f2( 0, 0 ) = "a" ; 
     f2( 1, 0 ) = "b" ; 
     f2( 0, 1 ) = "c" ; 
     f2( 1, 1 ) = "d" ; 
     fields["field<std::string>"] = f2 ;
-    field<colvec> f3(2,2) ;
-    f3(0,0) = zeros<mat>(5,1) ;
-    f3(1,0) = zeros<mat>(4,1) ;
-    f3(0,1) = zeros<mat>(3,1) ;
-    f3(1,1) = zeros<mat>(2,1) ;
+    
+    arma::field<arma::colvec> f3(2,2) ;
+    f3(0,0) = arma::zeros<arma::mat>(5,1) ;
+    f3(1,0) = arma::zeros<arma::mat>(4,1) ;
+    f3(0,1) = arma::zeros<arma::mat>(3,1) ;
+    f3(1,1) = arma::zeros<arma::mat>(2,1) ;
     fields["field<colvec>"] = f3 ;
 	
-    output[0] = matrices;
-    output[1] = rows ;
-    output[2] = cols ;
-    output[3] = fields ;
-	
-    names[0] = "matrices : Mat<T>" ; 
-    names[1] = "rows : Row<T>" ;
-    names.push_back( "columns : Col<T>" );
-    names.push_back( "fields  : field<T>" );
-    output.names() = names ;
-	
+    List output = List::create( 
+    	_["matrices : Mat<T>"]  = matrices, 
+    	_["rows : Row<T>"]      = rows,
+    	_["columns : Col<T>"]   = cols, 
+    	_["fields  : field<T>"] = fields ) ;
+    
     return output ;
 	
 }
@@ -118,12 +86,12 @@ extern "C" SEXP RcppArmadillo_as_Mat(SEXP input_){
     arma::umat m3 = input[0] ; /* implicit as */
     arma::fmat m4 = input[1] ; /* implicit as */
 	
-    List res(4) ;
-    res[0] = arma::accu( m1 ) ;
-    res[1] = arma::accu( m2 ) ;
-    res[2] = arma::accu( m3 ) ;
-    res[3] = arma::accu( m4 ) ;
-	
+    List res = List::create( 
+    	arma::accu( m1 ),
+    	arma::accu( m2 ),
+    	arma::accu( m3 ),
+    	arma::accu( m4 ) ) ;
+    
     return res ;
 }
 
@@ -134,12 +102,12 @@ extern "C" SEXP RcppArmadillo_as_Col( SEXP input_){
     arma::ucolvec m3 = input[0] ; /* implicit as */
     arma::fcolvec m4 = input[1] ; /* implicit as */
 	
-    List res(4) ;
-    res[0] = arma::accu( m1 ) ;
-    res[1] = arma::accu( m2 ) ;
-    res[2] = arma::accu( m3 ) ;
-    res[3] = arma::accu( m4 ) ;
-	
+    List res = List::create( 
+    	arma::accu( m1 ),
+    	arma::accu( m2 ),
+    	arma::accu( m3 ),
+    	arma::accu( m4 ) ) ;
+    
     return res ;
 }
 
@@ -150,32 +118,30 @@ extern "C" SEXP RcppArmadillo_as_Row(SEXP input_){
     arma::urowvec m3 = input[0] ; /* implicit as */
     arma::frowvec m4 = input[1] ; /* implicit as */
 	
-    List res(4) ;
-    res[0] = arma::accu( m1 ) ;
-    res[1] = arma::accu( m2 ) ;
-    res[2] = arma::accu( m3 ) ;
-    res[3] = arma::accu( m4 ) ;
-	
+    List res = List::create( 
+    	arma::accu( m1 ),
+    	arma::accu( m2 ),
+    	arma::accu( m3 ),
+    	arma::accu( m4 ) ) ;
     return res ;
-	
 }
 
 extern "C" SEXP RcppArmadillo_wrap_Glue(){
 	
-    arma::mat m1 = eye<mat>( 3, 3 ) ;
-    arma::mat m2 = eye<mat>( 3, 3 ) ;
-	
-    List res ;
+    arma::mat m1 = arma::eye<arma::mat>( 3, 3 ) ;
+    arma::mat m2 = arma::eye<arma::mat>( 3, 3 ) ;
+	                     
+    List res ;                                  
     res["mat+mat"] = m1 + m2 ;
     return res ;
 }
 
 extern "C" SEXP RcppArmadillo_wrap_Op(){
 
-    arma::mat m1 = eye<mat>( 3, 3 ) ;
+	arma::mat m1 = arma::eye<arma::mat>( 3, 3 ) ;
 	
     List res ;
-    res["mat+mat"] = - m1 ;
+    res["- mat"] = - m1 ;
     return res ;
 }
 
@@ -185,12 +151,11 @@ extern "C" SEXP armadillo_version(SEXP single_){
     if( single ){
 	return Rcpp::wrap( 10000*av.major + 100*av.minor + av.patch ) ;
     }
-    Rcpp::IntegerVector version(3); 
-    version = av.major, av.minor, av.patch ;
-    Rcpp::CharacterVector names(3); 
-    names = "major", "minor", "patch" ;
-    version.names() = names ;
-    version.attr("class" ) = "armadillo_version" ;
+    IntegerVector version = IntegerVector::create( 
+    	_["major"] = av.major, 
+    	_["minor"] = av.minor, 
+    	_["patch"] = av.patch
+    	) ;
     return version ;
 }
 
