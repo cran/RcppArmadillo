7259cd66e2bcad6d0d8cb415470c8bb48e4abf37
Dirk Eddelbuettel <edd@debian.org>
2013-04-19 00:00:00 +0000

    version 0.3.810.0

---
+++ inst/doc/RcppArmadillo-intro.R
@@ -0,0 +1,9 @@
+### R code from vignette source 'RcppArmadillo-intro.Rnw'
+
+###################################################
+### code chunk number 1: RcppArmadillo-intro.Rnw:44-46
+###################################################
+prettyVersion <- packageDescription("RcppArmadillo")$Version
+prettyDate <- format(Sys.Date(), "%B %e, %Y")
+
+
---
+++ inst/doc/RcppArmadillo-unitTests.R
@@ -0,0 +1,10 @@
+### R code from vignette source 'RcppArmadillo-unitTests.Rnw'
+
+###################################################
+### code chunk number 1: RcppArmadillo-unitTests.Rnw:13-16
+###################################################
+if( file.exists( "unitTests-results/RcppArmadillo-unitTests.txt" ) ){
+	writeLines( readLines( "unitTests-results/RcppArmadillo-unitTests.txt" ) )
+}
+
+
