5bcaede09b989f7b71031e2fb7ef594e26aee1d0
Dirk Eddelbuettel <edd@debian.org>
2013-08-15 12:59:31 +0000

    version 0.3.910.0

--- inst/doc/RcppArmadillo-intro.R
+++
@@ -1,9 +0,0 @@
-### R code from vignette source 'RcppArmadillo-intro.Rnw'
-
-###################################################
-### code chunk number 1: RcppArmadillo-intro.Rnw:44-46
-###################################################
-prettyVersion <- packageDescription("RcppArmadillo")$Version
-prettyDate <- format(Sys.Date(), "%B %e, %Y")
-
-
